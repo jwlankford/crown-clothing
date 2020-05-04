72f1ac2dd92ffdd711ad5075e8e04f5a7ad648a1
Jeremy Lankford <jwlankford@gmail.com>
2020-05-04 16:40:59 -0400

    added redux, and implemented userReducer and userActions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import './header.styles.scss'
-import { auth } from '../../firebase/firebase.utils'
-import { Link } from "react-router-dom"
-import { ReactComponent as Logo } from '../../assests/crownlogo.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { auth } from '../../firebase/firebase.utils';
+
+import { ReactComponent as Logo } from '../../assets/crownlogo.svg';
+
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/shop">CONTACT</Link>
-            {
-                currentUser ? 
-                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                    <Link className="option" to="/signin"> SIGN IN</Link>
-            }
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
-export default Header;
+export default connect(mapStateToProps)(Header);
