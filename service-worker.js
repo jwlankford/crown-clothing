3414b05810dc546832fc97649f182c8266dafaab
Jeremy Lankford <jwlankford@gmail.com>
2020-05-08 03:18:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crown-clothing/precache-manifest.b1e4ae201e66338332b9787d31fa2bdc.js"
+  "/crown-clothing/precache-manifest.b435f95db576be9f2b6c8982171efc40.js"
 );
 
 self.addEventListener('message', (event) => {
