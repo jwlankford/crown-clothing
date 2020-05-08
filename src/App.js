aa3bc90c2b65c26ff34b03e4c20318ed210065dc
Jeremy Lankford <jwlankford@gmail.com>
2020-05-08 02:57:28 -0400

    add new route to site to redirect crown-clothing.

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
+          <Route exact path='/crown-clothing' component={HomePage} />
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
