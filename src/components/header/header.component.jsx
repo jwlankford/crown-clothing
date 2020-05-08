200dcf91f9271a1e7e3b2920453e5c887a029d8d
Jeremy Lankford <jwlankford@gmail.com>
2020-05-08 03:32:04 -0400

    update.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,14 +15,14 @@ import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
-    <Link className='logo-container' to='/crown-clothing/'>
+    <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
-      <Link className='option' to='/crown-clothing/shop'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/crown-clothing/shop'>
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
@@ -30,7 +30,7 @@ const Header = ({ currentUser, hidden }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/crown-clothing/signin'>
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
       )}
