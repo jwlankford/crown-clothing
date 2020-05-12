a314a1bf507a82aaf52170a349d90cf77b0d178c
Jeremy Lankford <jwlankford@gmail.com>
2020-05-12 07:26:03 -0400

    update firebase calls

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,6 +7,8 @@ import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
+import './cart-icon.styles.scss'
+
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
