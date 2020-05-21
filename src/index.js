5172706a50fad72f287c15bde6b3d5e327315d41
Jeremy Lankford <jwlankford@gmail.com>
2020-05-20 20:49:32 -0400

    update the router to base.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/crown-clothing/">
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
